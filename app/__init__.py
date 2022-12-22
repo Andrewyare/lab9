3b2fecb7637d623548259039821f44c31a0c2c7d
andrewyare <91269160+Andrewyare@users.noreply.github.com>
2022-12-22 16:06:16 +0200

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
+from flask_login import LoginManager
+from config import config
+
+
+bcrypt = Bcrypt()
+app = Flask(__name__)
+login_manager = LoginManager(app)
+login_manager.login_view = 'account.login'
+login_manager.login_message_category = 'info'
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_name = 'default'):
+	app = Flask(__name__)
+    # setup with the configuration provided by the user / environment
+	app.config.from_object(config.get(config_name))
+    # setup all our dependencies
+	db.init_app(app)
+	migrate.init_app(app, db, render_as_batch=True)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+    
+	with app.app_context():
+		from app.home import home_bp
+		from app.form_cabinet import form_bp
+		from app.account import account_bp
+		
+		app.register_blueprint(home_bp)
+		app.register_blueprint(form_bp)
+		app.register_blueprint(account_bp)
+        
+	return app
